855051c964ab407083265d38abfc4c2620cf2923
DK\obr <olegbraginskyy@sitecore.net>
2018-04-20 16:19:50 +0300

    Add Switcher Event Disabler to Sync Bucket Process

---
+++ src/Sitecore.Support.452872/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.452872")]
+[assembly: AssemblyProduct("Sitecore.Support.452872")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.452872/RunSyncProcess.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.Support.Buckets.Pipelines.BucketOperations.SyncBucket
+{
+  using Sitecore.Buckets.Pipelines.BucketOperations.SyncBucket;
+  using Sitecore.Common;
+  using Sitecore.Data.Events;
+  using Sitecore.Data.Items;
+
+  public class RunSyncProcess : Sitecore.Buckets.Pipelines.BucketOperations.SyncBucket.RunSyncProcess
+  {
+    protected override Item CreateAndReturnDateFolderDestination(Item topParent, Item itemToMove)
+    {
+      Switcher<EventDisablerState, EventDisabler>.Enter(EventDisablerState.Disabled);
+      Item item = base.CreateAndReturnDateFolderDestination(topParent, itemToMove);
+      Switcher<EventDisablerState, EventDisabler>.Enter(EventDisablerState.Enabled);
+      return item;
+    }
+  }
+}
